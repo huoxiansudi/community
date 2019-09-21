623658b0b5bf516fb25a90ba5df5294b0f66fe74
huoxiansudi <=>
2019-09-21 17:47:30 +0800

    二级评论修改提交

--- src/main/resources/static/js/community.js
+++ src/main/resources/static/js/community.js
@@ -1,9 +1,11 @@
 /**
- * 提交回复
+ * 评论方法封装
+ * @param targetId
+ * @param type
+ * @param content
  */
-function post() {
-    var questionId = $("#question_id").val();
-    var content = $("#comment_content").val();
+
+function comment2Target(targetId, type, content) {
 
     if (!content) {
         alert("回复内容不能为空！");
@@ -14,9 +16,9 @@ function post() {
         type: "POST",
         url: "/comment",
         data: JSON.stringify({
-            "parentId": questionId,
+            "parentId": targetId,
             "content": content,
-            "type": 1
+            "type": type
         }),
         success: function (response) {
             if (response.code == 200) {
@@ -39,10 +41,22 @@ function post() {
         dataType: "json",
         contentType: "application/json"
     });
+}
 
+/**
+ * 提交回复
+ */
+function post() {
+    var questionId = $("#question_id").val();
+    var content = $("#comment_content").val();
 
-    console.log(questionId);
-    console.log(content);
+    comment2Target(questionId, 1, content);
+}
+
+function comment(e) {
+    var commentId = e.getAttribute("data-id");
+    var content = $("#input-" + commentId).val();
+    comment2Target(commentId, 2, content);
 }
 
 /**
@@ -60,10 +74,67 @@ function collapseComments(e) {
         e.removeAttribute("data-collapse");
         e.classList.remove("active");
     } else {
-        //展开二级评论
-        comments.addClass("in");
-        //标记二级评论展开状态
-        e.setAttribute("data-collapse", "in");
-        e.classList.add("active")
+
+        var subCommentContainer = $("#comment-" + id);
+        if (subCommentContainer.children().length != 1) {
+            //展开二级评论
+            comments.addClass("in");
+            //标记二级评论展开状态
+            e.setAttribute("data-collapse", "in");
+            e.classList.add("active");
+        } else {
+            $.getJSON("/comment/" + id, function (data) {
+                console.log(data);
+                var items = [];
+                $.each(data.data.reverse(), function (index, comment) {
+                    console.log(comment);
+
+                    /*var avatarElement = $("<img/>", {
+                        "class": "media-object img-circle",
+                        "src": comment.user.avatarUrl
+                    });*/
+
+                    var mediaLeftElement = $("<div/>", {
+                        "class": "media-left"
+                    }).append($("<img/>", {
+                        "class": "media-object img-circle",
+                        "src": comment.user.avatarUrl
+                    }));
+
+                    var mediaBodyElement = $("<div/>", {
+                        "class": "media-body"
+                    }).append($("<h5/>", {
+                        "class": "media-heading",
+                        "html": comment.user.name
+                    })).append($("<div/>", {
+                        "html": comment.content
+                    })).append($("<div/>", {
+                        "class": "menu"
+                    }).append($("<span/>", {
+                        "class": "pull-right",
+                        "html": moment(comment.gmtCreate).format("YYYY-MM-DD HH:mm:ss")
+                    })));
+
+                    var mediaElement = $("<div/>", {
+                        "class": "media"
+                    }).append(mediaLeftElement)
+                        .append(mediaBodyElement);
+
+                    var commentElement = $("<div/>", {
+                        "class": "col-lg-12 col-md-12 col-sm-12 col-xs-12 comments"
+                    }).append(mediaElement);
+
+                    subCommentContainer.prepend(commentElement);
+                });
+
+                //展开二级评论
+                comments.addClass("in");
+                //标记二级评论展开状态
+                e.setAttribute("data-collapse", "in");
+                e.classList.add("active");
+            });
+        }
+
+
     }
 }
