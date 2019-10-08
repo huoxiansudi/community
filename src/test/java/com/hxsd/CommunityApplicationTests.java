bc4a639ce09a54f129056e61f1e8b8a5ffc3e7e6
huoxiansudi <=>
2019-10-08 15:30:15 +0800

    日志修改

--- src/test/java/com/hxsd/CommunityApplicationTests.java
+++ src/test/java/com/hxsd/CommunityApplicationTests.java
@@ -4,8 +4,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -13,8 +18,11 @@ public class CommunityApplicationTests {
 
 	Logger logger = LoggerFactory.getLogger(getClass());
 
+	@Autowired
+	DataSource dataSource;
+
 	@Test
-	public void contextLoads() {
+	public void contextLoads() throws SQLException {
 
 		logger.trace("这是trace日志");
 		logger.debug("这是debug日志");
@@ -23,6 +31,13 @@ public class CommunityApplicationTests {
 		logger.info("这是info日志");
 		logger.warn("这是warn日志");
 		logger.error("这是error日志");
+
+		System.out.println(dataSource.getClass());
+
+		Connection connection = dataSource.getConnection();
+
+		System.out.println(connection);
+
 
 	}
 
